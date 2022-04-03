ba61b74256e9757ea47b4fff0daa9a2872633de9
Crocodil <evstarshov@icloud.com>
2022-04-03 19:10:25 +0300

    newNoteVC

---
+++ NoteApp/Controllers/NewNoteViewController.swift
@@ -0,0 +1,22 @@
+//
+//  NewNoteViewController.swift
+//  NoteApp
+//
+//  Created by Евгений Старшов on 03.04.2022.
+//
+
+import UIKit
+
+class NewNoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+
+    // MARK: - Navigation
+
+
+
+}
--- NoteApp/Controllers/NoteTableViewController.swift
+++ NoteApp/Controllers/NoteTableViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class NoteTableViewController: UITableViewController {
     
+    @IBOutlet weak var newNoteButton: UIBarButtonItem!
+    @IBOutlet weak var settingsButton: UIBarButtonItem!
+    
     var notes = [Note]()
     
     private struct Constants {
@@ -79,14 +82,13 @@ class NoteTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+     @IBAction func newNotebtnTapped() {
+         let newNoteVC = NewNoteViewController()
+         navigationController?.pushViewController(newNoteVC, animated: true)
+         newNoteVC.modalPresentationStyle = .fullScreen
+     }
 
 }
